d7be3f6ab637c8551a7ad627394d1cb748437550
kamil.lapinski <kamil.lapinski@britenet.com.pl>
2022-07-21 14:10:50 +0200

    added productService (products GET)

---
+++ js/productService.js
@@ -0,0 +1,48 @@
+const getProducts = () => {
+    return new Promise( (resolve, reject) => {
+        fetch('http://localhost:8080/api/v1/product')
+            .then( async response => {
+                const products = await response.json();
+                resolve(products);
+            } )
+            .catch( error => {
+                console.log('[DEBUG] ERROR');
+                console.log(JSON.stringify(error));
+                reject(error);
+            } );
+    } );
+}
+
+const renderProducts = (products) => {
+    const productsElement = document.querySelector('#products');
+    if (productsElement) {
+
+        products.forEach( product => {
+            productsElement.innerHTML += `
+                <div class="product">
+                    <img class="product__img" src="img/${product.images.paths}" alt="woman"/>
+                    <div class="product__info">
+                        <h3> ${product.name} </h3>
+                        <p> ${product.description} </p>
+                        <div class="product__actions">
+                            <p class="product__price"> ${product.price} PLN </p>
+                            <p class="product__buy"> KUP </p>
+                        </div>
+                    </div>
+                </div>
+            `
+        } );
+
+    }
+    else{
+        throw new Error('Cannot find #products.');
+    }
+}
+
+getProducts()
+.then( products => {
+    renderProducts(products);
+} )
+.catch( error => {
+    console.log(JSON.stringify(error));
+})
